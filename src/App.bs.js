38d2c6a4ba059fac7fa952ef72bfedd6078240d4
Endre Vegh <endre.vegh@yahoo.com>
2019-05-27 07:20:38 +0100

    add form sketch

--- src/App.bs.js
+++ src/App.bs.js
@@ -3,36 +3,148 @@
 
 var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
-var CamlinternalOO = require("bs-platform/lib/js/camlinternalOO.js");
+var Caml_builtin_exceptions = require("bs-platform/lib/js/caml_builtin_exceptions.js");
 
-var class_tables = [
-  0,
-  0,
-  0
+function Form(Config) {
+  var useForm = function (initialValues, onSubmit) {
+    var match = React.useReducer((function (_state, action) {
+            return action[0];
+          }), initialValues);
+    var dispatch = match[1];
+    var state = match[0];
+    var match$1 = React.useState((function () {
+            return Config[/* defaultStatus */0];
+          }));
+    var setFormStatus = match$1[1];
+    return /* record */[
+            /* values */state,
+            /* handleChange */(function (state) {
+                return Curry._1(dispatch, /* Update */[state]);
+              }),
+            /* status */match$1[0],
+            /* isSubmitting : NotSubmitting */1,
+            /* handleSubmit */(function (_event) {
+                return Curry._2(onSubmit, state, /* record */[/* setStatus */(function (status) {
+                                return Curry._1(setFormStatus, (function (param) {
+                                              return status;
+                                            }));
+                              })]);
+              })
+          ];
+  };
+  return /* module */[/* useForm */useForm];
+}
+
+function update(state, value) {
+  return /* record */[
+          /* username */value,
+          /* password */state[/* password */1]
+        ];
+}
+
+var UserNameField = /* module */[/* update */update];
+
+function update$1(state, value) {
+  return /* record */[
+          /* username */state[/* username */0],
+          /* password */value
+        ];
+}
+
+function validate(value) {
+  return "1".includes(value);
+}
+
+var PassWordField = /* module */[
+  /* update */update$1,
+  /* validate */validate
 ];
 
-function useForm(param) {
-  if (!class_tables[0]) {
-    var $$class = CamlinternalOO.create_table(0);
-    var env = CamlinternalOO.new_variable($$class, "");
-    var env_init = function (env$1) {
-      var self = CamlinternalOO.create_object_opt(0, $$class);
-      self[env] = env$1;
-      return self;
-    };
-    CamlinternalOO.init_class($$class);
-    class_tables[0] = env_init;
-  }
-  return Curry._1(class_tables[0], 0);
+var MyForm = /* module */[
+  /* UserNameField */UserNameField,
+  /* PassWordField */PassWordField,
+  /* defaultStatus : Ok */0
+];
+
+function useForm(initialValues, onSubmit) {
+  var match = React.useReducer((function (_state, action) {
+          return action[0];
+        }), initialValues);
+  var dispatch = match[1];
+  var state = match[0];
+  var match$1 = React.useState((function () {
+          return /* Ok */0;
+        }));
+  var setFormStatus = match$1[1];
+  return /* record */[
+          /* values */state,
+          /* handleChange */(function (state) {
+              return Curry._1(dispatch, /* Update */[state]);
+            }),
+          /* status */match$1[0],
+          /* isSubmitting : NotSubmitting */1,
+          /* handleSubmit */(function (_event) {
+              return Curry._2(onSubmit, state, /* record */[/* setStatus */(function (status) {
+                              return Curry._1(setFormStatus, (function (param) {
+                                            return status;
+                                          }));
+                            })]);
+            })
+        ];
+}
+
+var LoginForm = /* module */[/* useForm */useForm];
+
+function d(param) {
+  return fetch("https://jsonplaceholder.typicode.com/todos/1").then((function (prim) {
+                  return prim.json();
+                })).then((function (x) {
+                console.log(x);
+                return Promise.reject(Caml_builtin_exceptions.not_found);
+              }));
 }
 
 function App(Props) {
-  useForm(/* () */0);
-  return React.createElement("div", undefined, React.createElement("input", undefined));
+  var form = useForm(/* record */[
+        /* username */"sample",
+        /* password */"pass"
+      ], (function (_values, form) {
+          d(/* () */0).then((function (_x) {
+                    Curry._1(form[/* setStatus */0], /* Success */1);
+                    return Promise.resolve(" ");
+                  })).catch((function (error) {
+                  Curry._1(form[/* setStatus */0], /* Error */["aint the case"]);
+                  console.log(error);
+                  return Promise.resolve("");
+                }));
+          return /* () */0;
+        }));
+  var match = form[/* status */2];
+  return React.createElement("div", undefined, React.createElement("div", undefined, React.createElement("input", {
+                      name: "username",
+                      value: form[/* values */0][/* username */0],
+                      onChange: (function ($$event) {
+                          return Curry._1(form[/* handleChange */1], update(form[/* values */0], $$event.target.value));
+                        })
+                    })), React.createElement("div", undefined, React.createElement("input", {
+                      name: "password",
+                      type: "password",
+                      value: form[/* values */0][/* password */1],
+                      onChange: (function ($$event) {
+                          return Curry._1(form[/* handleChange */1], update$1(form[/* values */0], $$event.target.value));
+                        })
+                    })), typeof match === "number" ? (
+                match !== 0 ? React.createElement("div", undefined, "Good") : null
+              ) : React.createElement("div", undefined, "Bad stuff happened"), React.createElement("div", undefined, React.createElement("button", {
+                      onClick: form[/* handleSubmit */4]
+                    }, "submit")));
 }
 
 var make = App;
 
-exports.useForm = useForm;
+exports.Form = Form;
+exports.MyForm = MyForm;
+exports.LoginForm = LoginForm;
+exports.d = d;
 exports.make = make;
 /* react Not a pure module */
--- src/App.re
+++ src/App.re
@@ -1,8 +1,156 @@
-let useForm = () => {};
+module type Config = {
+  type state;
+  type field;
+  type formStatus;
+
+  let defaultStatus: formStatus;
+};
+
+module Form = (Config: Config) => {
+  type submission =
+    | Submitting
+    | NotSubmitting;
+
+  type formProps = {
+    values: Config.state,
+    handleChange: Config.state => unit,
+    status: Config.formStatus,
+    isSubmitting: submission,
+    handleSubmit: ReactEvent.Mouse.t => unit,
+  };
+
+  type action =
+    | Update(Config.state);
+
+  type formBag = {setStatus: Config.formStatus => unit};
+
+  let useForm =
+      (
+        ~initialValues: Config.state,
+        ~onSubmit: (Config.state, formBag) => unit,
+      )
+      : formProps => {
+    let (state, dispatch) =
+      React.useReducer(
+        (_state, action) =>
+          switch (action) {
+          | Update(state) => state
+          },
+        initialValues,
+      );
+
+    let (formStatus, setFormStatus) =
+      React.useState(() => Config.defaultStatus);
+    {
+      values: state,
+      status: formStatus,
+      isSubmitting: NotSubmitting,
+      handleChange: state => dispatch(Update(state)),
+      handleSubmit: _event => {
+        onSubmit(state, {setStatus: status => setFormStatus(_ => status)});
+      },
+    };
+  };
+};
+
+module MyForm = {
+  type state = {
+    username: string,
+    password: string,
+  };
+
+  type field =
+    | Username
+    | Password;
+
+  type formStatus =
+    | Ok
+    | Success
+    | Error(string);
+
+  module UserNameField = {
+    let update = (state, value) => {...state, username: value};
+  };
+
+  module PassWordField = {
+    let update = (state, value) => {...state, password: value};
+    let validate = value => value->Js.String.includes("1");
+  };
+
+  let defaultStatus = Ok;
+};
+
+module LoginForm = Form(MyForm);
+
+let d = () => {
+  Js.Promise.(
+    Fetch.fetch("https://jsonplaceholder.typicode.com/todos/1")
+    |> then_(Fetch.Response.json)
+    |> then_(x => {
+         Js.log(x);
+         Js.Promise.reject(Not_found);
+       })
+  );
+};
 
 [@react.component]
 let make = () => {
-  useForm()->ignore;
+  let form =
+    LoginForm.useForm(
+      ~initialValues={username: "sample", password: "pass"},
+      ~onSubmit=(_values, form) =>
+      Js.Promise.(
+        d()
+        |> then_(_x => {
+             form.setStatus(MyForm.Success);
+             resolve(" ");
+           })
+        |> catch(error => {
+             form.setStatus(MyForm.Error("aint the case"));
+             Js.log(error);
+             resolve("");
+           })
+      )
+      ->ignore
+    );
 
-  <div> <input /> </div>;
+  <div>
+    <div>
+      <input
+        name="username"
+        value={form.values.username}
+        onChange={event =>
+          form.handleChange(
+            MyForm.UserNameField.update(
+              form.values,
+              event->ReactEvent.Form.target##value,
+            ),
+          )
+        }
+      />
+    </div>
+    <div>
+      <input
+        type_="password"
+        name="password"
+        onChange={event =>
+          form.handleChange(
+            MyForm.PassWordField.update(
+              form.values,
+              event->ReactEvent.Form.target##value,
+            ),
+          )
+        }
+        value={form.values.password}
+      />
+    </div>
+    {switch (form.status) {
+     | MyForm.Success => <div> "Good"->React.string </div>
+     | MyForm.Error(_e) => <div> "Bad stuff happened"->React.string </div>
+     | _ => React.null
+     }}
+    <div>
+      <button onClick={form.handleSubmit}> "submit"->React.string </button>
+    </div>
+  </div>;
 };
